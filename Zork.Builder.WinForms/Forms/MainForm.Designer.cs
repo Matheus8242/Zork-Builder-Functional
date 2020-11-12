dbfd042e9a1b1e6dc587112034172a0b2625c047
Matheus1735 <c_matheus@live.com>
2020-11-12 16:03:11 -0500

    Zork Builder

--- Zork.Builder.WinForms/Forms/MainForm.Designer.cs
+++ Zork.Builder.WinForms/Forms/MainForm.Designer.cs
@@ -32,17 +32,18 @@
             this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
-            this.roomsTabPage = new System.Windows.Forms.TabPage();
-            this.roomsLabel = new System.Windows.Forms.Label();
+            this.inventoryBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.itemsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.playerTabPage = new System.Windows.Forms.TabPage();
+            this.nameListLabel = new System.Windows.Forms.Label();
+            this.roomsGroupBox = new System.Windows.Forms.GroupBox();
             this.deleteRoomButton = new System.Windows.Forms.Button();
             this.addRoomButton = new System.Windows.Forms.Button();
             this.roomsListBox = new System.Windows.Forms.ListBox();
-            this.playerTabPage = new System.Windows.Forms.TabPage();
             this.playerInventoryGroupBox = new System.Windows.Forms.GroupBox();
             this.deleteItemButton = new System.Windows.Forms.Button();
             this.addItemButton = new System.Windows.Forms.Button();
             this.playerInventoryListBox = new System.Windows.Forms.ListBox();
-            this.inventoryBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.playerScoreTextBox = new System.Windows.Forms.TextBox();
             this.playerHealthTextBox = new System.Windows.Forms.TextBox();
             this.playerNameTextBox = new System.Windows.Forms.TextBox();
@@ -52,17 +53,16 @@
             this.deletePlayerButton = new System.Windows.Forms.Button();
             this.addPlayerButton = new System.Windows.Forms.Button();
             this.playerListBox = new System.Windows.Forms.ListBox();
-            this.itemsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.mainTabControl = new System.Windows.Forms.TabControl();
             ((System.ComponentModel.ISupportInitialize)(this.worldViewModelBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.playersBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.roomsBindingSource)).BeginInit();
             this.mainMenuStrip.SuspendLayout();
-            this.roomsTabPage.SuspendLayout();
-            this.playerTabPage.SuspendLayout();
-            this.playerInventoryGroupBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.inventoryBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.itemsBindingSource)).BeginInit();
+            this.playerTabPage.SuspendLayout();
+            this.roomsGroupBox.SuspendLayout();
+            this.playerInventoryGroupBox.SuspendLayout();
             this.mainTabControl.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -86,7 +86,6 @@
             // 
             // mainMenuStrip
             // 
-            this.mainMenuStrip.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.mainMenuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
@@ -143,65 +142,20 @@
             this.saveFileDialog.Filter = "World Files(*json)|*.json";
             this.saveFileDialog.Title = "Save world file";
             // 
-            // roomsTabPage
+            // inventoryBindingSource
             // 
-            this.roomsTabPage.Controls.Add(this.roomsLabel);
-            this.roomsTabPage.Controls.Add(this.deleteRoomButton);
-            this.roomsTabPage.Controls.Add(this.addRoomButton);
-            this.roomsTabPage.Controls.Add(this.roomsListBox);
-            this.roomsTabPage.Location = new System.Drawing.Point(4, 29);
-            this.roomsTabPage.Name = "roomsTabPage";
-            this.roomsTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.roomsTabPage.Size = new System.Drawing.Size(839, 591);
-            this.roomsTabPage.TabIndex = 2;
-            this.roomsTabPage.Text = "Rooms";
-            this.roomsTabPage.UseVisualStyleBackColor = true;
+            this.inventoryBindingSource.DataMember = "Items";
+            this.inventoryBindingSource.DataSource = this.worldViewModelBindingSource;
             // 
-            // roomsLabel
+            // itemsBindingSource
             // 
-            this.roomsLabel.AutoSize = true;
-            this.roomsLabel.Location = new System.Drawing.Point(6, 23);
-            this.roomsLabel.Name = "roomsLabel";
-            this.roomsLabel.Size = new System.Drawing.Size(60, 20);
-            this.roomsLabel.TabIndex = 24;
-            this.roomsLabel.Text = "&Rooms";
-            // 
-            // deleteRoomButton
-            // 
-            this.deleteRoomButton.Location = new System.Drawing.Point(87, 516);
-            this.deleteRoomButton.Name = "deleteRoomButton";
-            this.deleteRoomButton.Size = new System.Drawing.Size(75, 36);
-            this.deleteRoomButton.TabIndex = 8;
-            this.deleteRoomButton.Text = "&Delete";
-            this.deleteRoomButton.UseVisualStyleBackColor = true;
-            this.deleteRoomButton.Click += new System.EventHandler(this.DeleteRoomButton_Click);
-            // 
-            // addRoomButton
-            // 
-            this.addRoomButton.Location = new System.Drawing.Point(6, 516);
-            this.addRoomButton.Name = "addRoomButton";
-            this.addRoomButton.Size = new System.Drawing.Size(75, 36);
-            this.addRoomButton.TabIndex = 7;
-            this.addRoomButton.Text = "&Add...";
-            this.addRoomButton.UseVisualStyleBackColor = true;
-            this.addRoomButton.Click += new System.EventHandler(this.AddRoomButton_Click);
-            // 
-            // roomsListBox
-            // 
-            this.roomsListBox.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.roomsBindingSource, "Name", true));
-            this.roomsListBox.DataSource = this.roomsBindingSource;
-            this.roomsListBox.DisplayMember = "Name";
-            this.roomsListBox.FormattingEnabled = true;
-            this.roomsListBox.ItemHeight = 20;
-            this.roomsListBox.Location = new System.Drawing.Point(6, 46);
-            this.roomsListBox.Name = "roomsListBox";
-            this.roomsListBox.Size = new System.Drawing.Size(250, 464);
-            this.roomsListBox.TabIndex = 6;
-            this.roomsListBox.ValueMember = "Name";
-            this.roomsListBox.SelectedIndexChanged += new System.EventHandler(this.RoomsListBox_SelectedIndexChanged);
+            this.itemsBindingSource.DataMember = "Items";
+            this.itemsBindingSource.DataSource = this.worldViewModelBindingSource;
             // 
             // playerTabPage
             // 
+            this.playerTabPage.Controls.Add(this.nameListLabel);
+            this.playerTabPage.Controls.Add(this.roomsGroupBox);
             this.playerTabPage.Controls.Add(this.playerInventoryGroupBox);
             this.playerTabPage.Controls.Add(this.playerScoreTextBox);
             this.playerTabPage.Controls.Add(this.playerHealthTextBox);
@@ -220,6 +174,61 @@
             this.playerTabPage.Text = "Player";
             this.playerTabPage.UseVisualStyleBackColor = true;
             // 
+            // nameListLabel
+            // 
+            this.nameListLabel.AutoSize = true;
+            this.nameListLabel.Location = new System.Drawing.Point(6, 23);
+            this.nameListLabel.Name = "nameListLabel";
+            this.nameListLabel.Size = new System.Drawing.Size(60, 20);
+            this.nameListLabel.TabIndex = 19;
+            this.nameListLabel.Text = "&Players";
+            // 
+            // roomsGroupBox
+            // 
+            this.roomsGroupBox.Controls.Add(this.deleteRoomButton);
+            this.roomsGroupBox.Controls.Add(this.addRoomButton);
+            this.roomsGroupBox.Controls.Add(this.roomsListBox);
+            this.roomsGroupBox.Location = new System.Drawing.Point(544, 222);
+            this.roomsGroupBox.Name = "roomsGroupBox";
+            this.roomsGroupBox.Size = new System.Drawing.Size(267, 288);
+            this.roomsGroupBox.TabIndex = 8;
+            this.roomsGroupBox.TabStop = false;
+            this.roomsGroupBox.Text = "Rooms";
+            // 
+            // deleteRoomButton
+            // 
+            this.deleteRoomButton.Location = new System.Drawing.Point(87, 235);
+            this.deleteRoomButton.Name = "deleteRoomButton";
+            this.deleteRoomButton.Size = new System.Drawing.Size(75, 36);
+            this.deleteRoomButton.TabIndex = 30;
+            this.deleteRoomButton.Text = "&Delete";
+            this.deleteRoomButton.UseVisualStyleBackColor = true;
+            this.deleteRoomButton.Click += new System.EventHandler(this.DeleteRoomButton_Click);
+            // 
+            // addRoomButton
+            // 
+            this.addRoomButton.Location = new System.Drawing.Point(6, 235);
+            this.addRoomButton.Name = "addRoomButton";
+            this.addRoomButton.Size = new System.Drawing.Size(75, 36);
+            this.addRoomButton.TabIndex = 29;
+            this.addRoomButton.Text = "&Add...";
+            this.addRoomButton.UseVisualStyleBackColor = true;
+            this.addRoomButton.Click += new System.EventHandler(this.AddRoomButton_Click);
+            // 
+            // roomsListBox
+            // 
+            this.roomsListBox.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.roomsBindingSource, "Name", true));
+            this.roomsListBox.DataSource = this.roomsBindingSource;
+            this.roomsListBox.DisplayMember = "Name";
+            this.roomsListBox.FormattingEnabled = true;
+            this.roomsListBox.ItemHeight = 20;
+            this.roomsListBox.Location = new System.Drawing.Point(6, 23);
+            this.roomsListBox.Name = "roomsListBox";
+            this.roomsListBox.Size = new System.Drawing.Size(241, 204);
+            this.roomsListBox.TabIndex = 28;
+            this.roomsListBox.ValueMember = "Name";
+            this.roomsListBox.SelectedIndexChanged += new System.EventHandler(this.RoomsListBox_SelectedIndexChanged);
+            // 
             // playerInventoryGroupBox
             // 
             this.playerInventoryGroupBox.Controls.Add(this.deleteItemButton);
@@ -227,7 +236,7 @@
             this.playerInventoryGroupBox.Controls.Add(this.playerInventoryListBox);
             this.playerInventoryGroupBox.Location = new System.Drawing.Point(243, 222);
             this.playerInventoryGroupBox.Name = "playerInventoryGroupBox";
-            this.playerInventoryGroupBox.Size = new System.Drawing.Size(295, 288);
+            this.playerInventoryGroupBox.Size = new System.Drawing.Size(250, 288);
             this.playerInventoryGroupBox.TabIndex = 18;
             this.playerInventoryGroupBox.TabStop = false;
             this.playerInventoryGroupBox.Text = "In&ventory";
@@ -260,15 +269,10 @@
             this.playerInventoryListBox.ItemHeight = 20;
             this.playerInventoryListBox.Location = new System.Drawing.Point(6, 25);
             this.playerInventoryListBox.Name = "playerInventoryListBox";
-            this.playerInventoryListBox.Size = new System.Drawing.Size(283, 204);
+            this.playerInventoryListBox.Size = new System.Drawing.Size(232, 204);
             this.playerInventoryListBox.TabIndex = 0;
             this.playerInventoryListBox.ValueMember = "Name";
             this.playerInventoryListBox.SelectedIndexChanged += new System.EventHandler(this.PlayerInventoryListBox_SelectedIndexChanged);
-            // 
-            // inventoryBindingSource
-            // 
-            this.inventoryBindingSource.DataMember = "Items";
-            this.inventoryBindingSource.DataSource = this.worldViewModelBindingSource;
             // 
             // playerScoreTextBox
             // 
@@ -347,22 +351,16 @@
             this.playerListBox.DisplayMember = "Name";
             this.playerListBox.FormattingEnabled = true;
             this.playerListBox.ItemHeight = 20;
-            this.playerListBox.Location = new System.Drawing.Point(0, 6);
+            this.playerListBox.Location = new System.Drawing.Point(0, 46);
             this.playerListBox.Name = "playerListBox";
-            this.playerListBox.Size = new System.Drawing.Size(233, 504);
+            this.playerListBox.Size = new System.Drawing.Size(233, 464);
             this.playerListBox.TabIndex = 1;
             this.playerListBox.ValueMember = "Name";
             this.playerListBox.SelectedIndexChanged += new System.EventHandler(this.PlayerListBox_SelectedIndexChanged);
             // 
-            // itemsBindingSource
-            // 
-            this.itemsBindingSource.DataMember = "Items";
-            this.itemsBindingSource.DataSource = this.worldViewModelBindingSource;
-            // 
             // mainTabControl
             // 
             this.mainTabControl.Controls.Add(this.playerTabPage);
-            this.mainTabControl.Controls.Add(this.roomsTabPage);
             this.mainTabControl.Location = new System.Drawing.Point(12, 36);
             this.mainTabControl.Name = "mainTabControl";
             this.mainTabControl.SelectedIndex = 0;
@@ -384,13 +382,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.roomsBindingSource)).EndInit();
             this.mainMenuStrip.ResumeLayout(false);
             this.mainMenuStrip.PerformLayout();
-            this.roomsTabPage.ResumeLayout(false);
-            this.roomsTabPage.PerformLayout();
-            this.playerTabPage.ResumeLayout(false);
-            this.playerTabPage.PerformLayout();
-            this.playerInventoryGroupBox.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.inventoryBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.itemsBindingSource)).EndInit();
+            this.playerTabPage.ResumeLayout(false);
+            this.playerTabPage.PerformLayout();
+            this.roomsGroupBox.ResumeLayout(false);
+            this.playerInventoryGroupBox.ResumeLayout(false);
             this.mainTabControl.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -409,11 +406,13 @@
         private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.SaveFileDialog saveFileDialog;
-        private System.Windows.Forms.TabPage roomsTabPage;
+        private System.Windows.Forms.BindingSource inventoryBindingSource;
+        private System.Windows.Forms.BindingSource itemsBindingSource;
+        private System.Windows.Forms.TabPage playerTabPage;
+        private System.Windows.Forms.GroupBox roomsGroupBox;
         private System.Windows.Forms.Button deleteRoomButton;
         private System.Windows.Forms.Button addRoomButton;
         private System.Windows.Forms.ListBox roomsListBox;
-        private System.Windows.Forms.TabPage playerTabPage;
         private System.Windows.Forms.GroupBox playerInventoryGroupBox;
         private System.Windows.Forms.Button deleteItemButton;
         private System.Windows.Forms.Button addItemButton;
@@ -428,9 +427,7 @@
         private System.Windows.Forms.Button addPlayerButton;
         private System.Windows.Forms.ListBox playerListBox;
         private System.Windows.Forms.TabControl mainTabControl;
-        private System.Windows.Forms.Label roomsLabel;
-        private System.Windows.Forms.BindingSource inventoryBindingSource;
-        private System.Windows.Forms.BindingSource itemsBindingSource;
+        private System.Windows.Forms.Label nameListLabel;
     }
 }
 
